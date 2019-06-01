bd3a5ec7e6c05c05e909293461511ecd9aa589fc
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-06-01 12:25:36 +0600

    ignore fix 2

--- api-keys.gradle.kts
+++
@@ -1,8 +0,0 @@
-allprojects{
-  extra.apply{
-    setProperty("twitter_api_key","otFHaJzItbvMwmkWihKjxu1dP")
-    setProperty("twitter_api_secret_key", "LDdv53vrwqZ1FLPd9kYMvhsniH7lmemYGCgxv38ITxBP8jI2S1")
-    setProperty("twitter_api_access_token", "3467191036-VzMNBxUd3mevTbW1z3yRIx2vingjHwzPJqx7vm5")
-    setProperty("twitter_api_access_token_secret", "2vT0kNjSfoHixUmXBO3IsjvHhGpRSvnpKvt0aociAtHkw")
-  }
-}
