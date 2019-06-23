c0a1e5b24487dd26d4864be0bbb8beb34ff2ff5e
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-06-23 13:59:18 +0600

    reddit datasourcefactory and repository impl

--- app/src/main/java/com/grappim/spacexapp/pagination/reddit/RedditDataSourceFactory.kt
+++ app/src/main/java/com/grappim/spacexapp/pagination/reddit/RedditDataSourceFactory.kt
@@ -1,2 +1,18 @@
 package com.grappim.spacexapp.pagination.reddit
 
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.grappim.spacexapp.model.reddit.RedditModel
+
+class RedditDataSourceFactory(
+  private val subreddit: String? = null
+) : DataSource.Factory<String, RedditModel>() {
+
+  val sourceLiveData = MutableLiveData<RedditDataSource>()
+
+  override fun create(): DataSource<String, RedditModel> {
+    val source = RedditDataSource(subreddit)
+    sourceLiveData.postValue(source)
+    return source
+  }
+}
--- app/src/main/java/com/grappim/spacexapp/repository/RedditRepositoryImpl.kt
+++ app/src/main/java/com/grappim/spacexapp/repository/RedditRepositoryImpl.kt
@@ -1,11 +1,34 @@
 package com.grappim.spacexapp.repository
 
-import com.grappim.spacexapp.model.reddit.RedditListingResponse
+import androidx.lifecycle.Transformations
+import androidx.paging.Config
+import androidx.paging.toLiveData
 import com.grappim.spacexapp.model.reddit.RedditModel
 import com.grappim.spacexapp.pagination.Listing
+import com.grappim.spacexapp.pagination.reddit.RedditDataSourceFactory
 
 class RedditRepositoryImpl : RedditRepository {
-  override fun getPostsBySubreddit(sureddit: String): Listing<RedditModel> {
-    TODO()
+  override fun getPostsBySubreddit(subreddit: String): Listing<RedditModel> {
+    val sourceFactory = RedditDataSourceFactory(subreddit)
+    val livePagedList = sourceFactory.toLiveData(
+      config = Config(
+        pageSize = 30,
+        enablePlaceholders = false,
+        initialLoadSizeHint = 60,
+        prefetchDistance = 20
+      )
+    )
+    return Listing(
+      pagedList = livePagedList,
+      networkState = Transformations.switchMap(
+        sourceFactory.sourceLiveData
+      ) {
+        it.networkState
+      },
+      refresh = { sourceFactory.sourceLiveData.value?.invalidate() },
+      failure = Transformations.switchMap(sourceFactory.sourceLiveData) {
+        it.failure
+      }
+    )
   }
 }
